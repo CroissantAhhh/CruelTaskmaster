acd2cdcf50360de420b63afda022c7acd0994deb
CroissantAhhh <jasonz4@uw.edu>
2021-11-27 14:46:07 -0800

    env page done styling pretty much

--- react-app/src/pages/EnvironmentPage/index.js
+++ react-app/src/pages/EnvironmentPage/index.js
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 
 import { loadEnvironmentJobs } from '../../store/jobs';
 import JobBoardCard from '../../components/JobBoardCard';
@@ -9,6 +9,7 @@ import "./EnvironmentPage.css";
 
 export default function EnvironmentPage() {
     const dispatch = useDispatch();
+    const history = useHistory();
     const [isLoaded, setIsLoaded] = useState(false);
     const { environmentHash } = useParams();
     const userEnvironments = useSelector(state => Object.values(state.environments));
@@ -26,23 +27,16 @@ export default function EnvironmentPage() {
     const envJobs = useSelector(state => Object.values(state.jobs));
     const [selectedJob, setSelectedJob] = useState();
 
-    // {
-    //     title: 'Sample Job Title',
-    //     banner: 'https://res.cloudinary.com/dmtj0amo0/image/upload/v1638048781/banners/banner11_oqou0s.jpg',
-    //     description: ' Sample Job Description',
-    //     sections: [
-    //         { id: 2341234, title: "To-do", taskCount: 10 },
-    //         { id: 5234523, title: "In Progress", taskCount: 2 },
-    //         { id: 5345234, title: "Complete", taskCount: 7 }
-    //     ]
-    // }
+    function goToJob() {
+        history.push(`/jobs/${selectedJob.hashedId}`)
+    }
 
     return (
         <div className="environment-page-container">
             {isLoaded && (
                 <div className="environment-page-content">
                     <div className="env-welcome-banner">
-                        <img src={currentEnvironment.banner} alt="env banner" height="500px" width="100%"></img>
+                        <img src={currentEnvironment.banner} alt="env banner" height="400px" width="100%"></img>
                         <div className="transparent-filter"></div>
                         <div className="env-welcome-text">
                             <p className="env-title">{currentEnvironment.title}</p>
@@ -65,10 +59,28 @@ export default function EnvironmentPage() {
                                 <p className="job-details-header">Job Details</p>
                                 {selectedJob && (
                                     <div className="job-details-info">
-                                        <img src={selectedJob?.banner} alt="job banner" height="300px" width="100%"></img>
-                                        <div className="transparent-filter"></div>
-                                        <p className="job-details-info-title">{selectedJob?.title}</p>
-                                        <p className="job-details-info-desc">{selectedJob?.description}</p>
+                                        <div className="job-details-top">
+                                            <img src={selectedJob?.banner} alt="job banner" height="300px" width="100%"></img>
+                                            <div className="transparent-filter"></div>
+                                            <p className="job-details-info-title">{selectedJob?.title}</p>
+                                            <p className="job-details-info-desc">{selectedJob?.description}</p>
+                                        </div>
+                                        <div className="job-details-bottom">
+                                            <ul className="tasks-breakdown">
+                                                <p className="tasks-breakdown-header">Tasks Status</p>
+                                                {selectedJob.tasksStatus.map((task) => (
+                                                    <li key={task.id} className="section-breakdown">
+                                                        <p>{`${task.title}: ${task.taskCount}`}</p>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                            <div className="job-board-portal" onClick={goToJob}>
+                                                <p>Go to Job Board</p>
+                                                <svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clipRule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </div>
                                     </div>
                                 )}
                             </div>
