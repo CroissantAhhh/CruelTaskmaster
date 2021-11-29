97c6333ba6eeb627ebebc830b12a9228f6d1f6bb
CroissantAhhh <jasonz4@uw.edu>
2021-11-29 13:48:46 -0800

    edit section modal style

--- react-app/src/components/JobBoard/JobBoardSection/index.js
+++ react-app/src/components/JobBoard/JobBoardSection/index.js
@@ -106,14 +106,16 @@ export default function JobBoardSection({ section, tasks, index }) {
             <div className="triangle-pointer"></div>
             <div className="EDS-content">
                 <button className="close-section-ED" onClick={e => closeSectionED(e)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="30px" width="30px" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                         <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
                     </svg>
                 </button>
-                <p className="edit-section-title">Edit Section Title</p>
-                <form className="edit-section-title-form" onSubmit={editSectionTitle}>
-                    <input type="text" value={sectionTitle} onChange={e => setSectionTitle(e.target.value)}></input>
-                </form>
+                <div className="edit-section-title-area">
+                    <p className="edit-section-title">Edit Section Title</p>
+                    <form className="edit-section-title-form" onSubmit={editSectionTitle}>
+                        <input type="text" value={sectionTitle} onChange={e => setSectionTitle(e.target.value)}></input>
+                    </form>
+                </div>
                 <DeleteConfirmationModal deleteRequest={deleteSection} resource={section} resourceName={section.title} />
             </div>
         </div>
